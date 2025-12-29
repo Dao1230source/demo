e90c1800388c420063c60e840910c532232169a4
Dao1230 <dao1230.source@gmail.com>
2025-12-30 01:56:58 +0800

    stream demo

--- spring-extension-starter/spring-extension-starter-stream/spring-extension-starter-stream-core/src/main/java/org/source/spring/stream/sfkafka/SfKafkaProducer.java
+++ spring-extension-starter/spring-extension-starter-stream/spring-extension-starter-stream-core/src/main/java/org/source/spring/stream/sfkafka/SfKafkaProducer.java
@@ -1,13 +1,20 @@
 package org.source.spring.stream.sfkafka;
 
-import com.sf.kafka.api.produce.KeyedBytes;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.sf.kafka.api.produce.KeyedString;
 import com.sf.kafka.api.produce.ProduceConfig;
 import com.sf.kafka.api.produce.ProduceOptionalConfig;
 import com.sf.kafka.api.produce.ProducerPool;
 import org.source.spring.stream.template.KafkaProducer;
+import org.source.utility.enums.BaseExceptionEnum;
+import org.source.utility.utils.Jsons;
 import org.springframework.messaging.Message;
+import org.springframework.util.StringUtils;
 
-public class SfKafkaProducer extends ProducerPool implements KafkaProducer<String, byte[]> {
+import java.util.List;
+
+public class SfKafkaProducer extends ProducerPool implements KafkaProducer<String, String> {
     private final String topic;
 
     public SfKafkaProducer(String topic, ProduceConfig config, ProduceOptionalConfig optionalConfig) {
@@ -26,18 +33,28 @@ public class SfKafkaProducer extends ProducerPool implements KafkaProducer<Strin
     }
 
     @Override
-    public byte[] getValue(Message<?> message) {
-        return (byte[]) message.getPayload();
-    }
-
-    @Override
-    public void send(String topic, String key, byte[] bytes) {
-        KeyedBytes keyedBytes = new KeyedBytes(key, bytes);
-        super.sendKeyedBytes(topic, keyedBytes);
-    }
-
-    @Override
-    public void send(String topic, byte[] bytes) {
-        super.sendBytes(topic, bytes);
+    public void send(Message<?> message) {
+        String payload = (String) message.getPayload();
+        try (JsonParser jsonParser = Jsons.getInstance().createParser(payload)) {
+            JsonToken jsonToken = jsonParser.nextToken();
+            String key = this.getKey(message);
+            if (jsonToken == JsonToken.START_OBJECT) {
+                if (StringUtils.hasText(key)) {
+                    this.sendKeyedString(this.getTopicName(), new KeyedString(key, payload));
+                } else {
+                    this.sendString(this.getTopicName(), payload);
+                }
+            } else {
+                if (StringUtils.hasText(key)) {
+                    List<KeyedString> keyedStringList = Jsons.list(payload, String.class)
+                            .stream().map(item -> new KeyedString(key, item)).toList();
+                    this.batchSendKeyedString(this.getTopicName(), keyedStringList);
+                } else {
+                    this.sendString(this.getTopicName(), payload);
+                }
+            }
+        } catch (Exception e) {
+            throw BaseExceptionEnum.JSON_STRING_2_OBJECT_EXCEPTION.except(e);
+        }
     }
 }
